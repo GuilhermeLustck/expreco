9a4f4cc69828261ef1cd6e664262f2edc660557f
Lustck <guidsouza10@hotmail.com>
2024-06-08 19:02:53 -0300

    foi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,14 @@
 export const environment = {
   production: false
 };
+const firebaseConfig = { 
+  apiKey : "AIzaSyBaRnNfUu-5gCWOP1ukvxWRwHNk4isXIlA" , 
+  authDomain : "bullet-express-44033.firebaseapp.com" , 
+  projectId : "bullet-express-44033" , 
+  storageBucket : "bullet-express-44033.appspot.com" , 
+  mensagensSenderId : "168471630434" , 
+  appId : "1:168471630434:web:04db4c00f8f65bd306373b" 
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
